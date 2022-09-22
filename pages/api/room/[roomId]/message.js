aca9ece51928766245c96aefa6474166b54da12f
ThankZ <natsuphat_th@cmu.ac.th>
2022-09-22 18:23:16 +0700

    Update

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,6 +4,18 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
+    const id = req.query.roomId;
+
+    const result1 = rooms.map((x) => {
+      if (x.roomId === id) {
+        const messages = x.messages;
+        return res.json({ ok: true, messages });
+      } else {
+        return res.status(404).json({ ok: false, message: "Invalid room Id" });
+      }
+    });
+
+    return result1;
   } else if (req.method === "POST") {
     const rooms = readDB();
 
